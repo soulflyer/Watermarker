de2095669f8f876b3c70a33e151f5ba3cdbe037d
Iain <public@soulflyer.co.uk>
2014-11-25 16:39:53 +0700

    Created applescript class to write IPTC info to a picture given its name

--- WaterMarker/AppDelegate.m
+++ WaterMarker/AppDelegate.m
@@ -7,6 +7,11 @@
 //
 
 #import "AppDelegate.h"
+@class IWWriteIPTC;
+
+@interface IWWriteIPTC : NSObject
+-(NSString *) writeIPTC:(NSString*)iptcData toField:(NSString*)iptcField ofPic:(NSString*)thePic ofProject:(NSString*)theProject ofMonth:(NSString*)theMonth ofYear:(NSString*)theYear;
+@end
 
 @interface AppDelegate ()
 
@@ -17,6 +22,9 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+  IWWriteIPTC *IWWriteIPTCInstance = [[NSClassFromString(@"IWWriteIPTC") alloc] init ];
+  NSString *result=[IWWriteIPTCInstance writeIPTC:@"BL66S15X1Y2" toField:@"SpecialInstructions" ofPic:@"DIW_0035" ofProject:@"04-Marshall" ofMonth:@"June" ofYear:@"2007"];
+  NSLog(@"%@",result);
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
