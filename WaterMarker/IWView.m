c307a4961d8eb07d320fad7c077ba8c4d65bad57
Iain <public@soulflyer.co.uk>
2014-12-06 23:03:56 +0700

    resize takes menu bar into account

--- WaterMarker/IWView.m
+++ WaterMarker/IWView.m
@@ -21,8 +21,13 @@
    
     //Set the window to the right proportions
     NSRect theWindowRect = [[self window] frame];
+    NSRect theWindowContentRect = [[self window] contentRectForFrameRect:theWindowRect];
     //theWindowRect.size.width = theWindowRect.size.height * image.size.width / image.size.height;
-    theWindowRect.size.height = theWindowRect.size.width * image.size.height / image.size.width;
+    //theWindowRect.size.height = theWindowRect.size.width * image.size.height / image.size.width;
+    theWindowContentRect.size.height = theWindowContentRect.size.width * image.size.height / image.size.width;
+    [[self window] setContentSize:theWindowContentRect.size];
+    
+    theWindowRect = [[self window] frame];
     if(theWindowRect.size.height > visibleScreen.size.height){
       theWindowRect.size.height=visibleScreen.size.height;
       theWindowRect.size.width=theWindowRect.size.height * image.size.width / image.size.height;
@@ -32,10 +37,13 @@
       theWindowRect.size.height = theWindowRect.size.width * image.size.height / image.size.width;
     }
     
-    [[self window] setContentSize:theWindowRect.size];
+    [[self window] setFrame:theWindowRect display:false];
+    theWindowContentRect = [[self window] contentRectForFrameRect:theWindowRect];
+    
+    //[[self window] setContentSize:theWindowRect.size];
 
     //Set the view to match the available space
-    NSRect theViewRect = theWindowRect;
+    NSRect theViewRect = theWindowContentRect;
     theViewRect.origin = NSZeroPoint;
     [self setFrame:theViewRect];
     
